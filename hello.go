4ce0594d5c6a0072ee6239a84efc722f6b5d7bdc
Greg Whorley <gwhorley@tripwire.com>
2020-04-26 14:19:34 -0700

    use remote module

--- hello.go
+++ hello.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/gregwhorley/go-go-garbage-project/morestrings"
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	fmt.Printf(morestrings.ReverseRunes("!oG olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
