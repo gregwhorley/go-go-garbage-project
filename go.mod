2538d67cae2f002079e779501dacc6a8ff58aabe
Greg Whorley <gwhorley@tripwire.com>
2020-04-26 13:55:19 -0700

    hello world shit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gregwhorley/go-go-garbage-project
+
+go 1.14
---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gregwhorley/morestrings"
+)
+
+type Balls int
+
+func main() {
+	fmt.Printf("the number 10 converted to Balls type is %d", Balls(10))
+}
